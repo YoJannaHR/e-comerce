e144081c271b0700b2c69c589d3a37dea605b529
brunoz <zegb94@gmail.com>
2022-06-14 17:02:09 -0500

    Se añadió el fitrado para los productos con el eventListtener en las categorias menos en la de show all BRUNO

--- assets/main.js
+++ assets/main.js
@@ -116,7 +116,62 @@ for(let i = 0; i < ropa.length; i++){
 
   /*!---=============INICIO DEL FILTRADO DEL PRODUCTOS=============---*/
 
-  function filtrarProductos( tipo ){
+  
+  let btnShowAll = document.querySelector(".show-all")
+  btnShowAll.addEventListener("click", () =>
+{
+  let contenedor1 = document.querySelector(".contenido_productos")
+  let fragmento = new DocumentFragment
+
+  for(let i = 0; i < ropa.length; i++) {
+
+    let article = document.createElement("article")
+    article.classList.add("card_productos%")//el div padre
+
+    let div = document.createElement("div")
+    div.classList.add("productos_imagenes")
+    let img = document.createElement("img")
+    img.src = ropa[i].imgUrl//terminan el div al que meto la imagen
+
+    let div2 = document.createElement("div2")
+    div2.classList.add("products__data")
+    
+    let h2 = document.createElement("h2")
+    h2.textContent = ropa[i].Precio
+    
+    let h3 = document.createElement("h3")
+    p.textContent = ropa[i].nombre
+    
+    article.appendChild(div)
+    div.appendChild(img)
+    article.appendChild(div2)
+    div2.appendChild(h2)
+    div2.appendChild(h3)
+    
+    fragmento.appendChild( article )
+}
+
+console.log(contenedor1.appendChild( fragmento ))
+})
+  let btnHoodies = document.querySelector(".hoodies")
+  btnHoodies.addEventListener("click", () =>
+{
+  return filtrado("hoodies")
+})
+let btnShirts = document.querySelector(".shirts")
+btnShirts.addEventListener("click", () =>
+{
+return filtrado("shirts")
+})
+let btnSweatshirts = document.querySelector(".sweatshirts")
+btnSweatshirts.addEventListener("click", () =>
+{
+return filtrado("sweatshirts")
+})
+
+
+
+  let filtrado = function filtrarProductos( tipo ){
 
 
     let contenedor = document.querySelector(".contenido_productos")
@@ -143,12 +198,8 @@ for(let i = 0; i < ropa.length; i++){
         `
     })
     contenedor.innerHTML = fragmento
-
-   
-
-
 }
-filtrarProductos("hoodies")
+
 
 /*!---=============FIN DEL FILTRADO DEL PRODUCTOS=============---*/
 /*!---=============INICIA EVANTO FILTRADO CON CLICK=============---*/
